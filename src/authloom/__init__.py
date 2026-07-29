7e9fb0ade267ba54895a81d18e702626f8af9a6a
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-07-30 01:32:09 +0530

    🔧 chore(test): setup conftest for integration tests

--- src/authloom/__init__.py
+++ src/authloom/__init__.py
@@ -1,7 +1,8 @@
 from authloom.router import create_auth_router
-from authloom.service import AuthLoom
+from authloom.service import AuthLoom, AuthLoomConfig
 
 __all__ = [
     "AuthLoom",
+    "AuthLoomConfig",
     "create_auth_router",
 ]
