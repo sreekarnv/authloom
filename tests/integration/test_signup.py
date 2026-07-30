d459affefa2f747a11e5f7b4b3efe33824b04390
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-07-30 15:01:10 +0530

    ✅ test(auth): add positive test case for signup

--- tests/integration/test_signup.py
+++ tests/integration/test_signup.py
@@ -2,6 +2,8 @@ import pytest
 from fastapi import FastAPI, status
 from httpx import ASGITransport, AsyncClient
 
+from authloom.exceptions import PasswordPolicyCode
+
 
 @pytest.mark.asyncio
 async def test_signup_rejects_with_same_email(app: FastAPI):
@@ -22,7 +24,6 @@ async def test_signup_rejects_with_same_email(app: FastAPI):
     assert second_response.status_code == status.HTTP_409_CONFLICT
     assert second_response.json()["detail"] == "user with this email already exists"
 
-
 @pytest.mark.asyncio
 async def test_signup_rejects_with_invalid_passwords(app: FastAPI):
     transport = ASGITransport(app=app)
@@ -37,3 +38,26 @@ async def test_signup_rejects_with_invalid_passwords(app: FastAPI):
         response = await client.post("/auth/signup", json=body)
 
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+    data = response.json()
+    assert "detail" in data and "message" in data["detail"]
+    assert data["detail"]["code"] == PasswordPolicyCode.TOO_SHORT
+
+@pytest.mark.asyncio
+async def test_signup_accepts_creates_user_and_cookie(app: FastAPI):
+    transport = ASGITransport(app=app)
+    body = {
+        "name": "Test User",
+        "email": "test_signup_accepts_creates_user_and_cookie@example.com",
+        "password": "#Test12234567890#",
+        "password_confirm": "#Test12234567890#"
+    }
+
+    async with AsyncClient(transport=transport, base_url="http://testserver") as client:
+        response = await client.post("/auth/signup", json=body)
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.cookies.get("authloom.auth")
+
+    data = response.json()
+    assert "user" in data and "message" in data
